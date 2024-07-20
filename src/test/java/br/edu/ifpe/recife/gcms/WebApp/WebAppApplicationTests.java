a5fa278e2420ec023c7c09ab13e52e2bf2bfd18f
Ramide Dantas <ramidedantas@recife.ifpe.edu.br>
2024-07-19 21:16:26 -0300

    1o commit de código

---
+++ src/test/java/br/edu/ifpe/recife/gcms/WebApp/WebAppApplicationTests.java
@@ -0,0 +1,39 @@
+package br.edu.ifpe.recife.gcms.WebApp;
+
+import org.junit.jupiter.api.Test; 
+import org.springframework.boot.test.context.SpringBootTest; 
+ 
+import static org.hamcrest.Matchers.containsString; 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get; 
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print; 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content; 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status; 
+ 
+import org.springframework.beans.factory.annotation.Autowired; 
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc; 
+import org.springframework.test.web.servlet.MockMvc; 
+ 
+@SpringBootTest 
+@AutoConfigureMockMvc 
+class WebAppApplicationTests { 
+ 
+  @Autowired 
+  private MockMvc mockMvc; 
+ 
+  @Test 
+  public void shouldReturnDefaultMessage() throws Exception { 
+    this.mockMvc.perform(get("/")) 
+        .andDo(print()).andExpect(status().isOk()) 
+        .andExpect(content() 
+          .string(containsString("Hello, World"))); 
+  } 
+  
+  @Test 
+  public void shouldReturnFulano() throws Exception { 
+    this.mockMvc.perform(get("/fulano")) 
+        .andDo(print()).andExpect(status().isOk()) 
+        .andExpect(content() 
+          .string(containsString("Hello, Fulano"))); 
+  } 
+ 
+}
