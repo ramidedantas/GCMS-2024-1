13660d90dd6af5202f0c75bca5d53902e4783acf
Richardson Silva <richardsonsilva97@hotmail.com>
2024-08-01 19:27:26 -0300

    Adicionando o Richardson no HomeController e ajustando indentação.
    Adicionando o Richardson no HomeController e ajustando indentação.

--- src/main/java/br/edu/ifpe/recife/gcms/WebApp/HomeController.java
+++ src/main/java/br/edu/ifpe/recife/gcms/WebApp/HomeController.java
@@ -5,65 +5,59 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class HomeController
-{
+public class HomeController {
     @RequestMapping("/")
-    public @ResponseBody String greeting()
-    {
+    public @ResponseBody String greeting() {
         return "Hello, World!!";
     }
 
     @RequestMapping("/fulano")
-    public @ResponseBody String greetingFulano()
-    {
+    public @ResponseBody String greetingFulano() {
         return "Hello, Fulano!";
     }
 
     @RequestMapping("/main")
-    public @ResponseBody String greetingMain()
-    {
+    public @ResponseBody String greetingMain() {
         return "Hello, Main!";
     }
 
     @RequestMapping("/hotfix")
-    public @ResponseBody String greetingHotfix()
-    {
+    public @ResponseBody String greetingHotfix() {
         return "Hello, Hotfix!";
     }
 
     @RequestMapping("/albert")
-    public @ResponseBody String greetingAlbert()
-    {
+    public @ResponseBody String greetingAlbert() {
         return "Hello, Albert!";
     }
 
     @RequestMapping("/branch/albert-alvin")
-    public @ResponseBody String greetingFromBranchAlbertAlvin()
-    {
+    public @ResponseBody String greetingFromBranchAlbertAlvin() {
         return "Hello from branch albert-alvin!";
     }
 
     @RequestMapping("/fork/albert")
-    public @ResponseBody String greetingForkAlbert()
-    {
+    public @ResponseBody String greetingForkAlbert() {
         return "Hello from Albert's fork!";
     }
 
     @RequestMapping("/JoaoGouveia")
-    public @ResponseBody String greetingJoaoGouveia()
-    {
+    public @ResponseBody String greetingJoaoGouveia() {
         return "Hello, João Gouveia!";
     }
 
     @RequestMapping("/rinaldo")
-    public @ResponseBody String greetingRinaldo()
-    {
+    public @ResponseBody String greetingRinaldo() {
         return "Hello, Rinaldo!";
     }
-    
+
     @RequestMapping("/Diogo")
-    public @ResponseBody String greetingDiogo()
-    {
+    public @ResponseBody String greetingDiogo() {
         return "Hello, Diogo!";
+    }
+
+    @RequestMapping("/richardson")
+    public @ResponseBody String greetingRichardson() {
+        return "Hello, Richardson Silva!";
     }
 }
